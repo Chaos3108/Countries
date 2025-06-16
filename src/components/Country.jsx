9d0ba511be69a95b1b4a012a3d581b231fc484fd
yuva kishore <yuvakishorekolli@gmail.com>
2025-06-16 11:46:33 +0530

    Added Search

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -5,26 +5,41 @@ import "./Country.css";
 
 const Country = () => {
   const [countriesData, setCountriesData] = useState([]);
-  const fetchCountries = async () => {
-    try {
-      const response = await axios.get(
-        " https://xcountries-backend.azurewebsites.net/all"
-      );
-      console.log(response.data);
-      setCountriesData(response.data);
-    } catch (error) {
-      console.error("Error fetching data:", error.message); // Explicit error message
-    }
-  };
+  const [filterData, setFilterData] = useState([]);
+  
+ const fetchCountries = async () => {
+  try {
+    const response = await axios.get(
+      " https://xcountries-backend.azurewebsites.net/all"
+    );
+    console.log(response.data);
+    setCountriesData(response.data);
+    setFilterData(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error.message); 
+  }
+};
   useEffect(() => {
     fetchCountries();
   }, []);
+ const handleSearch = (event) => {
+  const searchTerm = event.target.value.toLowerCase();
+  if (!searchTerm) {
+    setFilterData(countriesData); 
+    return;
+  }
+  const filteredCountries = countriesData.filter((country) =>
+    country.name.toLowerCase().includes(searchTerm)
+  );
+  setFilterData(filteredCountries);
+}
   return (
     <div className="country-container">
       <h1>Countries</h1>
+      <input onChange={(e)=>{handleSearch(e)}} type="text" placeholder="Search for countries" className="search-input" />
       <div className="container">
-        {countriesData &&
-          countriesData.map((item) => {
+        {filterData &&
+          filterData.map((item) => {
             return <CountryCard name={item.name} imageUrl={item.flag} />;
           })}
       </div>
